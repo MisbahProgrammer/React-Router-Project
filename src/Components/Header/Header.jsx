0808f07375071efaadefab4d12b544ce5250bbd1
MisbahProgrammer <misbahrehman891@gmail.com>
2024-09-25 09:33:53 +0500

    updated Header& Home page

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link to="/" className="flex items-center">
                         <img
-                            src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+                            src="/logo4.png"
                             className="mr-3 h-12"
                             alt="Logo"
                         />
@@ -22,7 +22,7 @@ function Header() {
                         </Link>
                         <Link
                             to="#"
-                            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                            className="text-white bg-cyan-500 hover:bg-cyan-700 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
                             Get started
                         </Link>
@@ -36,7 +36,7 @@ function Header() {
                                 <NavLink
                                 to='/'
                                     className={({isActive}) =>
-                                        `${isActive ? "text-orange-700" : "text-gray-700"} block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                        `${isActive ? "text-cyan-500" : "text-gray-700"} block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-cyan-500 lg:p-0`
                                     }
                                 >
                                     Home
@@ -46,7 +46,7 @@ function Header() {
                                 <NavLink
                                 to='/about'
                                     className={({isActive}) =>
-                                        `${isActive ? "text-orange-700" : "text-gray-700"} block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                        `${isActive ? "text-cyan-500" : "text-gray-700"} block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-cyan-600 lg:p-0`
                                     }
                                 >
                                     About
@@ -56,7 +56,7 @@ function Header() {
                                 <NavLink
                                 to='/contact'
                                     className={({isActive}) =>
-                                        `${isActive ? "text-orange-700" : "text-gray-700"} block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                        `${isActive ? "text-cyan-500" : "text-gray-700"} block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-cyan-600 lg:p-0`
                                     }
                                 >
                                     Contact
@@ -66,7 +66,7 @@ function Header() {
                                 <NavLink
                                 to='/github'
                                     className={({isActive}) =>
-                                        `${isActive ? "text-orange-700" : "text-gray-700"} block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                        `${isActive ? "text-cyan-500" : "text-gray-700"} block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-cyan-600 lg:p-0`
                                     }
                                 >
                                     GitHub
