121aa08981c1ebb5acd28620c812a12a0aea1ced
MisbahProgrammer <misbahrehman891@gmail.com>
2024-09-30 17:04:36 +0500

    modify about,home,contact section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -4,46 +4,52 @@ function About() {
   return (
     <div className="py-16 bg-white">
             <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
-                <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+                <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center md:justify-center lg:gap-12">
                 <div class="flex-1 p-4 md:w-3/5">
-                <p class="mb-10 md:max-w-2xl">I am  a Frontend Developer  with over a year  of professional expertise in the field.Throughout my career, I have had the privilege of collaborating with prestigious organizations, contributing to their success and growth.</p>
+                <p class="mb-10 md:max-w-4xl  mx-auto text-2xl font-mono">I am  a Frontend Developer  with over a year  of professional expertise in the field.Throughout my career, I have had the privilege of collaborating with prestigious organizations, contributing to their success and growth.</p>
 
                 <ul class="grid grid-cols-1 md:grid-cols-3 gap-6">
                     <li class="border-[0.5px] border-gray-400 rounded-xl p-4 cursor-pointer hover:bg-[#fcf4ff] hover:-translate-y-1 duration-500 hover:shadow-[4px_4px_0_#000]">
                         <i class="fa-solid fa-code text-3xl mt-4"></i>
-                        <h3 class="my-4 font-semibold text-gray-700">Languages</h3>
-                        <p class="text-gray-600 text-sm break-words">HTML-CSS-JAVASCRIPT-BOOTSRAP-TAILWIND CSS-REACT JS and Wordpress</p>
+                        <h3 class="my-4 font-semibold text-gray-700 text-2xl">Languages</h3>
+                        <p class="text-gray-600 break-words text-xl">HTML-CSS-JAVASCRIPT-BOOTSRAP-TAILWIND CSS-REACT JS and Wordpress</p>
                     </li>
                     <li class="border-[0.5px] border-gray-400 rounded-xl p-4 cursor-pointer hover:bg-[#fcf4ff] hover:-translate-y-1 duration-500 hover:shadow-[4px_4px_0_#000]">
                         <i class="fa-solid fa-graduation-cap text-3xl mt-4"></i>
-                        <h3 class="my-4 font-semibold text-gray-700">Education</h3>
-                        <p class="text-gray-600 text-sm">Studying in Bachelors in Computer Science [1st Semester]</p>
+                        <h3 class="my-4 font-semibold text-gray-700 text-2xl">Education</h3>
+                        <p class="text-gray-600 text-xl">Studying in Bachelors in Computer Science [1st Semester]</p>
                     </li>
                     <li class="border-[0.5px] border-gray-400 rounded-xl p-4 cursor-pointer hover:bg-[#fcf4ff] hover:-translate-y-1 duration-500 hover:shadow-[4px_4px_0_#000]">
                         <i class="fa-solid fa-check-to-slot text-3xl mt-4"></i>
-                        <h3 class="my-4 font-semibold text-gray-700">Projects</h3>
-                        <p class="text-gray-600 text-sm">Built more than 5 Projects.</p>
+                        <h3 class="my-4 font-semibold text-gray-700 text-2xl">Projects</h3>
+                        <p class="text-gray-600  text-xl">Built more than 5 Projects.</p>
                     </li>
             </ul>
-            <h4 class="mt-4 mb-4 font-semibold">Tools I Use</h4>
-            <ul class="flex items-center gap-3 md:gap-6">
-                <li class="flex items-center justify-center w-12 md:w-14 aspect-square cursor-pointer border border-gray-400 rounded-lg hover:-translate-y-1 duration-500">
-                    
+            <h4 class="mt-10 mb-4 font-semibold text-3xl">Tools I Use</h4>
+            <ul class="flex items-center gap-3 md:gap-6 mx-auto justify-center md:mt-10">
+                <li class="flex items-center justify-center w-12 md:w-16 aspect-square cursor-pointer border border-gray-400 rounded-lg hover:-translate-y-1 duration-500">
+                    <img src="vscode.png" alt="" className='p-2'/>
                 </li>
-                <li class="flex items-center justify-center w-12 md:w-14 aspect-square cursor-pointer border border-gray-400 rounded-lg hover:-translate-y-1 duration-500">
-                    
+                <li class="flex items-center justify-center w-12 md:w-16 aspect-square cursor-pointer border border-gray-400 rounded-lg hover:-translate-y-1 duration-500">
+                    <img src="github.svg" alt=""  className='p-2'/>
                 </li>
-                <li class="flex items-center justify-center w-12 md:w-14 aspect-square cursor-pointer border border-gray-400 rounded-lg hover:-translate-y-1 duration-500">
-                    
+                <li class="flex items-center justify-center w-12 md:w-16 aspect-square cursor-pointer border border-gray-400 rounded-lg hover:-translate-y-1 duration-500">
+                    <img src="jest.png" alt=""  className='p-2'/>
                 </li>
-                <li class="flex items-center justify-center w-12 md:w-14 aspect-square cursor-pointer border border-gray-400 rounded-lg hover:-translate-y-1 duration-500">
-                    
+                <li class="flex items-center justify-center w-12 md:w-16 aspect-square cursor-pointer border border-gray-400 rounded-lg hover:-translate-y-1 duration-500">
+                   <img src="vercel.svg" alt=""  className='p-2'/> 
                 </li>
-                <li class="flex items-center justify-center w-12 md:w-14 aspect-square cursor-pointer border border-gray-400 rounded-lg hover:-translate-y-1 duration-500">
-                    
+                <li class="flex items-center justify-center w-12 md:w-16 aspect-square cursor-pointer border border-gray-400 rounded-lg hover:-translate-y-1 duration-500">
+                    <img src="vitejs.svg" alt=""  className='p-2'/>
                 </li>
-                <li class="flex items-center justify-center w-12 md:w-14 aspect-square cursor-pointer border border-gray-400 rounded-lg hover:-translate-y-1 duration-500">
-            
+                <li class="flex items-center justify-center w-12 md:w-16 aspect-square cursor-pointer border border-gray-400 rounded-lg hover:-translate-y-1 duration-500">
+                    <img src="wordpress.png" alt=""  className='p-2'/>
+                </li>
+                <li class="flex items-center justify-center w-12 md:w-16 aspect-square cursor-pointer border border-gray-400 rounded-lg hover:-translate-y-1 duration-500">
+                    <img src="canva.jfif" alt=""  className='p-2'/>
+                </li>
+                <li class="flex items-center justify-center w-12 md:w-16 aspect-square cursor-pointer border border-gray-400 rounded-lg hover:-translate-y-1 duration-500">
+                    <img src="cypress.webp" alt=""  className='p-2'/>
                 </li>
             </ul>
             </div>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -40,47 +40,34 @@ function Home() {
         <img className="sm:w-96 w-48 rounded-lg" src="/coding.jpg" alt="image2" />
     </div>
 
-    <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Languages</h1>
-    <div class="flex-1 p-4 md:w-3/5">
-                <p class="mb-10 md:max-w-2xl">I am  a Frontend Developer  with over a year  of professional expertise in the field.Throughout my career, I have had the privilege of collaborating with prestigious organizations, contributing to their success and growth.</p>
-
-                <ul class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    <li class="border-[0.5px] border-gray-400 rounded-xl p-4 cursor-pointer hover:bg-[#fcf4ff] hover:-translate-y-1 duration-500 hover:shadow-[4px_4px_0_#000]">
-                        <i class="fa-solid fa-code text-3xl mt-4"></i>
-                        <h3 class="my-4 font-semibold text-gray-700">Languages</h3>
-                        <p class="text-gray-600 text-sm break-words">HTML-CSS-JAVASCRIPT-BOOTSRAP-TAILWIND CSS-REACT JS and Wordpress</p>
-                    </li>
-                    <li class="border-[0.5px] border-gray-400 rounded-xl p-4 cursor-pointer hover:bg-[#fcf4ff] hover:-translate-y-1 duration-500 hover:shadow-[4px_4px_0_#000]">
-                        <i class="fa-solid fa-graduation-cap text-3xl mt-4"></i>
-                        <h3 class="my-4 font-semibold text-gray-700">Education</h3>
-                        <p class="text-gray-600 text-sm">Studying in Bachelors in Computer Science [1st Semester]</p>
-                    </li>
-                    <li class="border-[0.5px] border-gray-400 rounded-xl p-4 cursor-pointer hover:bg-[#fcf4ff] hover:-translate-y-1 duration-500 hover:shadow-[4px_4px_0_#000]">
-                        <i class="fa-solid fa-check-to-slot text-3xl mt-4"></i>
-                        <h3 class="my-4 font-semibold text-gray-700">Projects</h3>
-                        <p class="text-gray-600 text-sm">Built more than 5 Projects.</p>
-                    </li>
-            </ul>
-            <h4 class="mt-4 mb-4 font-semibold">Tools I Use</h4>
-            <ul class="flex items-center gap-3 md:gap-6">
-                
-                <li class="flex items-center justify-center w-12 md:w-14 aspect-square cursor-pointer border border-gray-400 rounded-lg hover:-translate-y-1 duration-500">
-        
-                </li>
-                <li class="flex items-center justify-center w-12 md:w-14 aspect-square cursor-pointer border border-gray-400 rounded-lg hover:-translate-y-1 duration-500">
-        
-                </li>
-                <li class="flex items-center justify-center w-12 md:w-14 aspect-square cursor-pointer border border-gray-400 rounded-lg hover:-translate-y-1 duration-500">
-        
-                </li>
-                <li class="flex items-center justify-center w-12 md:w-14 aspect-square cursor-pointer border border-gray-400 rounded-lg hover:-translate-y-1 duration-500">
-        
-                </li>
-                <li class="flex items-center justify-center w-12 md:w-14 aspect-square cursor-pointer border border-gray-400 rounded-lg hover:-translate-y-1 duration-500">
-
-                </li>
-            </ul>
+    {/* --------------------------Services---------------------------------------- */}
+    <div id="services" class="w-full md:scroll-mt-20 px-[12%] py-10">
+        <h4 class="text-center text-2xl mb-2">What i offer</h4>
+        <h2 class="text-center text-5xl font-semibold ">My Services</h2>
+        <p class="text-center max-w-2xl mx-auto mt-5 mb-12 text-xl">I am a Frontend Developer from Pakistan, with over a year of experience in a startup and on freelancing platforms like Guru,Freelancer,People per hour. </p>
+        <div class="grid grid-cols-3  gap-6 my-10">
+            <div class=" flex flex-col items-center justify-center border border-gray-400 rounded-lg  px-8 py-12 hover:shadow-[4px_4px_0_#000] cursor-pointer hover:bg-[#fcf4ff] hover:-translate-y-1 duration-500">
+                <img src="webdev.png" alt="" className='w-60'/>
+                <h3 class="font-semibold text-xl">Web Development</h3>
+                <p>Create custom websites from scratch or redesign existing ones.</p>
+                <a href="" class="flex items-center align-middle gap-2 text-xl mt-4 text-cyan-700">Read more <i class="fa-solid fa-arrow-right w-4 pt-1 "></i></a>
             </div>
+            <div class="flex flex-col items-center justify-center border border-gray-400 rounded-lg px-8 py-12 hover:shadow-[4px_4px_0_#000] cursor-pointer hover:bg-[#fcf4ff] hover:-translate-y-1 duration-500">
+                <img src="wordpress.png" alt="" className='w-60'/>
+                <h3 class="font-semibold text-xl">Wordpress Development</h3>
+                <p>Creating and customizing WordPress websites using themes, plugins, and custom code.</p>
+                <a href="" class="flex items-center align-middle gap-2 text-xl mt-4 text-cyan-700">Read more <i class="fa-solid fa-arrow-right w-4 pt-1 "></i></a>
+            </div>
+           
+            <div class="flex flex-col items-center justify-center border border-gray-400 rounded-lg px-8 py-12 hover:shadow-[4px_4px_0_#000] cursor-pointer hover:bg-[#fcf4ff] hover:-translate-y-1 duration-500">
+                <img src="code-audit.png" alt=""  className='w-60'/>
+                <h3 class="font-semibold text-xl">Frontend Code Audit</h3>
+                <p>Review and analyze existing frontend code to identify areas for improvement and optimization.</p>
+                <a href="" class="flex items-center align-middle gap-2 text-xl mt-4 text-cyan-700">Read more <i class="fa-solid fa-arrow-right w-4 pt-1 "></i></a>
+            </div>
+        </div>
+     </div>
+    
 </div>
   )
 }
